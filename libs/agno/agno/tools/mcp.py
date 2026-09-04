3a29d3af4a916c41dfc269730b4f7f8702502667
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-04 16:17:01 -0400

    Muna4029__diff__id_158

--- libs/agno/agno/tools/mcp.py
+++ libs/agno/agno/tools/mcp.py
@@ -164,13 +164,13 @@ class MCPTools(Toolkit):
             sse_params = asdict(self.server_params) if self.server_params is not None else {}
             if "url" not in sse_params:
                 sse_params["url"] = self.url
-            self._context = sse_client(**sse_params)
+            self._context = sse_client(**sse_params)  # type: ignore
             client_timeout = min(self.timeout_seconds, sse_params.get("timeout", self.timeout_seconds))
         elif self.transport == "streamable-http":
             streamable_http_params = asdict(self.server_params) if self.server_params is not None else {}
             if "url" not in streamable_http_params:
                 streamable_http_params["url"] = self.url
-            self._context = streamablehttp_client(**streamable_http_params)
+            self._context = streamablehttp_client(**streamable_http_params)  # type: ignore
             client_timeout = min(self.timeout_seconds, streamable_http_params.get("timeout", self.timeout_seconds))
         else:
             if self.server_params is None:
