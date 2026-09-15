2f5e6e185748ab318d2a74f88b663c9cb2666c2b
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-15 19:11:26 -0400

    Muna4029__diff__id_167

--- libs/agno/agno/tools/firecrawl.py
+++ libs/agno/agno/tools/firecrawl.py
@@ -8,7 +8,11 @@ from agno.utils.log import logger
 try:
     from firecrawl import FirecrawlApp, ScrapeOptions  # type: ignore[attr-defined]
 except ImportError:
-    raise ImportError("`firecrawl-py` not installed. Please install using `pip install firecrawl-py`")
+    try:
+        from firecrawl import FirecrawlApp, V1ScrapeOptions as ScrapeOptions  # type: ignore[attr-defined]
+    except ImportError:
+        FirecrawlApp = None  # type: ignore
+        ScrapeOptions = None  # type: ignore
 
 
 class CustomJSONEncoder(json.JSONEncoder):
