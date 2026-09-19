87608063b7bc0a4339c14297a78d0a44c8f850ca
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-19 14:17:15 -0400

    Muna4029__diff__id_73

--- libs/agno/agno/models/cerebras/cerebras.py
+++ libs/agno/agno/models/cerebras/cerebras.py
@@ -13,10 +13,10 @@ from agno.models.response import ModelResponse
 from agno.utils.log import log_error, log_warning
 
 try:
-    from cerebras.cloud.sdk import AsyncCerebras as AsyncCerebrasClient
-    from cerebras.cloud.sdk import Cerebras as CerebrasClient
-    from cerebras.cloud.sdk.types.chat import ChatCompletion
-    from cerebras.cloud.sdk.types.chat.chat_completion import (
+    from cerebras.cloud.sdk import AsyncCerebras as AsyncCerebrasClient  # type: ignore[import-not-found]
+    from cerebras.cloud.sdk import Cerebras as CerebrasClient  # type: ignore[import-not-found]
+    from cerebras.cloud.sdk.types.chat import ChatCompletion  # type: ignore[import-not-found]
+    from cerebras.cloud.sdk.types.chat.chat_completion import (  # type: ignore[import-not-found]
         ChatChunkResponse,
         ChatChunkResponseChoice,
         ChatChunkResponseChoiceDelta,
@@ -260,13 +260,14 @@ class Cerebras(Model):
             **self.get_request_kwargs(response_format=response_format, tools=tools),
         )  # type: ignore
 
-    async def ainvoke_stream(
+    async def ainvoke_stream(  # type: ignore[override]
         self,
         messages: List[Message],
         response_format: Optional[Union[Dict, Type[BaseModel]]] = None,
         tools: Optional[List[Dict[str, Any]]] = None,
         tool_choice: Optional[Union[str, Dict[str, Any]]] = None,
-    ) -> AsyncIterator[ChatChunkResponse]:
+    ) -> AsyncIterator[Any]:
+
         """
         Sends an asynchronous streaming chat completion request to the Cerebras API.
 
