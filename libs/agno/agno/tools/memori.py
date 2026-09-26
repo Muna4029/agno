393a5b1ec65a6bda7462f68f796489a91e4d8597
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 21:34:26 -0400

    Muna4029__diff__id_296

--- libs/agno/agno/tools/memori.py
+++ libs/agno/agno/tools/memori.py
@@ -6,7 +6,8 @@ from agno.tools.toolkit import Toolkit
 from agno.utils.log import log_debug, log_error, log_info, log_warning
 
 try:
-    from memori import Memori, create_memory_tool
+    from memori import Memori
+    from memori.tools import create_memory_tool
 except ImportError:
     raise ImportError("`memorisdk` package not found. Please install it with `pip install memorisdk`")
 
